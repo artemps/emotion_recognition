045dd1f9396c3e2c098ba0eef9f677a6f88c4287
Artem Pshenichny <fised@mail.ru>
2018-04-06 16:39:18 +0300

    Optimization. Added extra images.

--- prepare_train_data.py
+++ prepare_train_data.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import glob
 import numpy as np
 from PIL import Image
 import pandas as pd
@@ -14,25 +15,48 @@ class PrepareData:
         if os.path.exists(os.path.join(os.getcwd(), DATA_DIR)):
             print('Dataset dir already exists.')
             return
-        else:
-            print('Start creating dataset...')
-            PrepareData._make_dirs()
+
+        print('Creating dataset...')
+        PrepareData._make_dirs()
 
         data = pd.read_csv(CSV_FILE_NAME)
         i = 0
         total = data.shape[0]
+        detector = Detector()
         for index, row in data.iterrows():
             emotion = row['emotion']
             image = PrepareData._data_to_image(row['pixels'])
 
             gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            detector = Detector()
             detected_faces = detector.detect_faces(gray_image)
-            aligned_faces, _ = detector.align_faces(gray_image, detected_faces)
+            aligned_faces = detector.align_faces(gray_image, detected_faces)
             for x in aligned_faces:
                 cv2.imwrite('{}/{}/{}.png'.format(DATA_DIR, EMOTIONS[emotion], i), x)
                 i += 1
                 print('Progress: {}/{} {:.2f}%'.format(i, total, i * 100.0 / total))
+
+    @staticmethod
+    def add_extra_images():
+        if not os.path.exists(os.path.join(os.getcwd(), EXTRA_DIR)):
+            print('Extra images dir does not exist')
+            return
+
+        print('Adding extra images...')
+        for emotion in EMOTIONS:
+            files = glob.glob('{}\\{}\\*'.format(EXTRA_DIR, emotion))
+
+            i = 0
+            total = len(files)
+            detector = Detector()
+            for f in files:
+                image = cv2.imread(f)
+                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                detected_faces = detector.detect_faces(gray_image)
+                aligned_faces = detector.align_faces(gray_image, detected_faces)
+                for x in aligned_faces:
+                    cv2.imwrite('{}/{}/extra_{}.png'.format(DATA_DIR, EMOTIONS[emotion], i), x)
+                    i += 1
+                    print('Progress: {}/{} {:.2f}%'.format(i, total, i * 100.0 / total))
 
     @staticmethod
     def _data_to_image(data):
