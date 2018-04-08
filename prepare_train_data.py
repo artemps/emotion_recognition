93fe2ac3d1b5d3d8f59586b3e5c80fb09212b4dc
Artem Pshenichny <fised@mail.ru>
2018-04-08 17:02:19 +0300

    Added info prints

--- prepare_train_data.py
+++ prepare_train_data.py
@@ -13,10 +13,10 @@ class PrepareData:
     @staticmethod
     def create_dataset_from_csv():
         if os.path.exists(os.path.join(os.getcwd(), DATA_DIR)):
-            print('Dataset dir already exists.')
+            print('\nDataset dir already exists.')
             return
 
-        print('Creating dataset...')
+        print('\nCreating dataset...')
         PrepareData._make_dirs()
 
         data = pd.read_csv(CSV_FILE_NAME)
@@ -38,23 +38,24 @@ class PrepareData:
     @staticmethod
     def add_extra_images():
         if not os.path.exists(os.path.join(os.getcwd(), EXTRA_DIR)):
-            print('Extra images dir does not exist')
+            print('\nExtra images dir does not exist')
             return
 
-        print('Adding extra images...')
-        for emotion in EMOTIONS:
-            files = glob.glob('{}\\{}\\*'.format(EXTRA_DIR, emotion))
+        detector = Detector()
 
+        print('\nAdding extra images...')
+        for emotion in EMOTIONS:
+            print('\nEmotion', emotion)
+            files = glob.glob('{}\\{}\\*'.format(EXTRA_DIR, emotion))
             i = 0
             total = len(files)
-            detector = Detector()
             for f in files:
                 image = cv2.imread(f)
                 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 detected_faces = detector.detect_faces(gray_image)
                 aligned_faces = detector.align_faces(gray_image, detected_faces)
                 for x in aligned_faces:
-                    cv2.imwrite('{}/{}/extra_{}.png'.format(DATA_DIR, EMOTIONS[emotion], i), x)
+                    cv2.imwrite('{}/{}/extra_{}.png'.format(DATA_DIR, emotion, i), x)
                     i += 1
                     print('Progress: {}/{} {:.2f}%'.format(i, total, i * 100.0 / total))
 
@@ -67,6 +68,7 @@ class PrepareData:
 
     @staticmethod
     def _make_dirs():
+        print('Creating dirs...\n')
         os.mkdir(os.path.join(os.getcwd(), DATA_DIR))
         for emotion in EMOTIONS:
             os.mkdir(os.path.join(os.getcwd(), os.path.join(DATA_DIR, emotion)))
