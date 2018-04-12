656654eb5e13302ba935fadf85127b0ae4471ea3
Artem Pshenichny <fised@mail.ru>
2018-04-12 15:11:27 +0300

    Update Report class. Add func desc.

--- report.py
+++ report.py
@@ -1,3 +1,6 @@
+__author__ = 'Artem Pshenichny'
+
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
@@ -7,17 +10,25 @@ from constants import *
 
 
 class Report:
+    """
+    Class for generating a reports after finish
+    """
+
     def __init__(self, date, times, predictions):
         self.date = date
         self.times = times
         self.predictions = predictions
 
     def create_line_chart(self):
-        # Выбираем значения каждой эмоции из всех предсказаний
+        """
+        Creates a line chart to shows emotions values in time
+        :return:
+        """
+
         plots = [[] for _ in range(0, len(EMOTIONS))]
         for i in range(0, len(EMOTIONS)):
             for p in self.predictions:
-                plots[i].append(p[0][i])
+                plots[i].append(p[i])
 
         fig, ax = plt.subplots()
         for emotion, plot in zip(EMOTIONS, plots):
@@ -33,14 +44,19 @@ class Report:
         plt.show()
 
     def create_time_line(self):
+        """
+        Creates a times line chart to shows time of emotion
+        :return:
+        """
+
         colors = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'magenta']
         emotion_colors = {e: c for e, c in zip(EMOTIONS, colors)}
 
         x = list(range(0, len(self.times)))
         e, y, c = [], [], []
         for p in self.predictions:
-            i = np.argmax(p[0])
-            y.append(p[0][i])
+            i = np.argmax(p)
+            y.append(p[i])
             e.append(EMOTIONS[i])
             c.append(emotion_colors[EMOTIONS[i]])
 
@@ -62,9 +78,14 @@ class Report:
         plt.show()
 
     def create_bar_chart(self):
+        """
+        Creates a bar chart to shows peaks emotions values in bar form
+        :return:
+        """
+
         emotions_count = {x: 0 for x in EMOTIONS}
         for p in self.predictions:
-            i = np.argmax(p[0])
+            i = np.argmax(p)
             emotions_count[EMOTIONS[i]] += 1
 
         fig, ax = plt.subplots()
@@ -81,9 +102,13 @@ class Report:
         plt.show()
 
     def create_pie_chart(self):
+        """
+        Creates a pie chart to show peaks emotions values in pie from
+        :return:
+        """
         emotions_count = {x: 0 for x in EMOTIONS}
         for p in self.predictions:
-            i = np.argmax(p[0])
+            i = np.argmax(p)
             emotions_count[EMOTIONS[i]] += 1
 
         sum_picks = sum(emotions_count.values())
@@ -99,4 +124,9 @@ class Report:
         plt.show()
 
     def create_csv(self):
+        """
+        Creates a csv file with times and emotions values
+        :return:
+        """
+
         pass
