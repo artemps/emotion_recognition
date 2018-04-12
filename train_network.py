d00897d4cda7bdc91156aa8013103ffbc7ebda7f
Artem Pshenichny <fised@mail.ru>
2018-04-12 15:06:10 +0300

    New CNN architecture. Added method for split data

--- train_network.py
+++ train_network.py
@@ -1,98 +1,113 @@
-import glob
-import cv2
-import random
+__author__ = 'Artem Pshenichny'
+
+
 import numpy as np
+import os
 import tflearn
 
-from tflearn.data_utils import shuffle, to_categorical
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 from tflearn.data_augmentation import ImageAugmentation
-from tflearn.data_preprocessing import ImagePreprocessing
 
 from constants import *
 
 
 class TrainNetwork:
-    def __init__(self):
-        self.train_images = []
-        self.train_labels = []
-        self.test_images = []
-        self.test_labels = []
+    """
+    Trainer class for train CNN
+    """
+
+    def __init__(self, images=None, emotions=None):
+        if not images or not emotions:
+            self.train_data = None
+            self.val_data = None
+        else:
+            self.train_data, self.val_data = TrainNetwork.split_data(np.asarray(images), np.asarray(emotions), 0.2)
+
         self.network = None
+        self.model = None
 
     def define_network(self):
-        # # Для нормализации данных(масштабирование и отцентровка данных)
+        """
+        Defines CNN architecture
+        :return: CNN model
+        """
+
+        # # For data normalization
         # img_prep = ImagePreprocessing()
         # img_prep.add_featurewise_zero_center()
         # img_prep.add_featurewise_stdnorm()
 
-        # Для создания дополнительных синтетических данных/увеличения кол-ва данных
-        # перевернутые, повернутые, размытые изображения
+        # For creating extra data(increase dataset). Flipped, Rotated, Blurred and etc. images
         img_aug = ImageAugmentation()
         img_aug.add_random_flip_leftright()
         img_aug.add_random_rotation(max_angle=25.0)
         img_aug.add_random_blur(sigma_max=3.0)
 
         self.network = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], data_augmentation=img_aug)
-
-        self.network = conv_2d(self.network, 64, 3, activation='relu')
-        self.network = max_pool_2d(self.network, 2)
-        self.network = conv_2d(self.network, 128, 3, activation='relu')
-        self.network = conv_2d(self.network, 128, 3, activation='relu')
-        self.network = max_pool_2d(self.network, 2)
+        self.network = conv_2d(self.network, 64, 5, activation='relu')
+        self.network = max_pool_2d(self.network, 3, strides=2)
+        self.network = conv_2d(self.network, 64, 5, activation='relu')
+        self.network = max_pool_2d(self.network, 3, strides=2)
+        self.network = conv_2d(self.network, 128, 4, activation='relu')
+        self.network = dropout(self.network, 0.3)
         self.network = fully_connected(self.network, 1024, activation='relu')
-        self.network = dropout(self.network, 0.5)
         self.network = fully_connected(self.network, len(EMOTIONS), activation='softmax')
-        self.network = regression(self.network, optimizer='adam',
-                                  loss='categorical_crossentropy', learning_rate=0.001)
-        model = tflearn.DNN(self.network, tensorboard_verbose=0,
-                            checkpoint_path='checkpoints/emotion-recognizer.tfl.ckpt')
+        self.network = regression(self.network, optimizer='momentum', loss='categorical_crossentropy')
+        self.model = tflearn.DNN(self.network, checkpoint_path=os.path.join(CHECKPOINTS_PATH + '/emotion_recognition'),
+                                 max_checkpoints=1, tensorboard_verbose=0)
 
-        return model
+        return self.model
 
-    def start_fit(self, model):
+    def start_train(self):
+        """
+        Starts train defined model
+        :return:
+        """
+
         print('\nEmotions:', EMOTIONS)
         print('Start training...')
 
-        (x, y), (x_test, y_test) = self.make_sets()
-        x, y = shuffle(x, y)
-        x_text, y_test = shuffle(x_test, y_test)
-        y = to_categorical(y, len(EMOTIONS))
-        y_test = to_categorical(y_test, len(EMOTIONS))
+        if not self.train_data or not self.val_data:
+            print('\nDataset is empty. Exit.')
+            exit()
 
-        model.fit(x, y, n_epoch=150, shuffle=True, validation_set=(x_test, y_test),
-                  show_metric=True, batch_size=50, snapshot_epoch=True, run_id='emotion-recognizer')
+        self.define_network()
+        self.model.fit(self.train_data[0], self.train_data[1], validation_set=self.val_data,
+                       n_epoch=100, batch_size=50, shuffle=True, show_metric=True,
+                       snapshot_epoch=True, snapshot_step=200, run_id='emotion-recognition')
 
-        model.save('model/emotion_recognizer.tfl')
-        print('\nNetwork trained and saved as emotion_recognizer.tfl')
+        self.model.save(os.path.join(MODEL_DIR, 'emotion_recognizer'))
+        print('\nNetwork trained and saved as emotion_recognizer')
 
-    def make_sets(self):
-        for emotion in EMOTIONS:
-            train, test = TrainNetwork.get_files(emotion)
-            for item in train:
-                image = cv2.imread(item)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255.
-                self.train_images.append(image)
-                self.train_labels.append(EMOTIONS.index(emotion))
+    def load_trained_model(self):
+        """
+        Loads trained model from save dir
+        :return:
+        """
 
-            for item in test:
-                image = cv2.imread(item)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255.
-                self.test_images.append(image)
-                self.test_labels.append(EMOTIONS.index(emotion))
-
-        self.train_images = np.reshape(self.train_images, [-1, IMG_SIZE, IMG_SIZE, 1])
-        self.test_images = np.reshape(self.test_images, [-1, IMG_SIZE, IMG_SIZE, 1])
-
-        return (self.train_images, self.train_labels), \
-               (self.test_images, self.test_labels)
+        model = self.define_network()
+        return model.load(os.path.join(MODEL_DIR, 'emotion_recognizer'))
 
     @staticmethod
-    def get_files(emotion):
-        files = glob.glob('{}\\{}\\*'.format(DATA_DIR, emotion))
-        random.shuffle(files)
-        train = files[:int(len(files) * 0.8)]  # Берем первые 80% файлов эмоции для тренировки
-        test = files[-int(len(files) * 0.2):]  # и последние 20% для тестирования
-        return train, test
+    def split_data(images, emotions, validation_split=0.2):
+        """
+        Splits input data to train and val
+        :param images: array of input images
+        :param emotions: array of input labels
+        :param validation_split: val data size from train data
+        :return: tuple of tuples (train_x, train_y), (val_x, val_y)
+        """
+
+        num_samples = len(images)
+        num_train_samples = int((1 - validation_split) * num_samples)
+        train_x = images[:num_train_samples]
+        train_y = emotions[:num_train_samples]
+        val_x = images[num_train_samples:]
+        val_y = emotions[num_train_samples:]
+
+        train_data = (train_x.reshape([-1, IMG_SIZE, IMG_SIZE, 1]), train_y)
+        val_data = (val_x.reshape([-1, IMG_SIZE, IMG_SIZE, 1]), val_y)
+
+        return train_data, val_data
